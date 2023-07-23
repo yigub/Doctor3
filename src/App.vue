7587fdb7c74b6d230192769543a0ac05737774e4
lww <2539849575@qq.com>
2023-07-23 23:35:50 +0800

    在third分支app.vue里加上了注释

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,6 @@
 // 这里是在进行了stores统一导出之后，一个比较简洁的写法。之前是import {useUserStore} from './stores/user.ts'
 // import { useUserStore } from './stores'
 </script>
+<!-- 我在third上写了这一段注释，third分支是基于main分支创建的 -->
 
 <style scoped></style>
