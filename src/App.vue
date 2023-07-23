81f3f3f186fa87ccc671018c4fdb08def2e25d97
lww <2539849575@qq.com>
2023-07-23 23:27:06 +0800

    在app.vue里加入了测试分支几个字

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,6 @@
 // 这里是在进行了stores统一导出之后，一个比较简洁的写法。之前是import {useUserStore} from './stores/user.ts'
 // import { useUserStore } from './stores'
 </script>
+<!-- 测试分支second -->
 
 <style scoped></style>
